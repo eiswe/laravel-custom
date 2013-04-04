1598ea585b25db37c804110210bf7ed7061569bd
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-04 20:17:32 +0200

    fixed syntax

--- application/models/page.php
+++ application/models/page.php
@@ -7,7 +7,7 @@ class Page extends Eloquent {
 	public static $timestamps = true;
 
 	public function admin() {
-		return $this->belongs_to('Admin')
+		return $this->belongs_to('Admin');
 	}
 	public function userid() {
 		return $this->has_one('Admin', 'userid');
