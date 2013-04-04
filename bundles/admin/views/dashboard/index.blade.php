cb252e85d24cce2617a216406ce427627c14f060
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-04 18:36:08 +0200

    added missing url var

--- bundles/admin/views/dashboard/index.blade.php
+++ bundles/admin/views/dashboard/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('subnav')
 	<?php 
+      $url = URL::base();
     	print '<li class="active">	<a href="'.$url.'/admin/home">          Home      </a></li>';
       print '<li>                 <a href="'.$url.'/admin/emacs">         Emacs  </a></li>';
       print '<li>                 <a href="'.$url.'/admin/page/list">     Pages  </a></li>';
