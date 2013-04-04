ef5fa37df0ee527480038b9b24a04415712c35b2
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-04 14:33:35 +0200

    i known i will forget to add files...

---
+++ application/views/layouts/navi.blade.php
@@ -0,0 +1,41 @@
+@layout('layouts.default')
+
+@section('topnav')
+
+  <?php 
+      $url = URL::base(); // http://laravel.dev       //   return the Base URL for Developing from different Servers
+      
+      $navlink = array(
+                array('Home', $url.'/home', true),
+                array('David', $url.'/david'),
+                array('Paolo', $url.'/paolo'),
+                array('Kazo', $url.'/kazo'),
+                array( 'Dropdown', '#', false, false, array(
+                    array('Action', '#'),
+                    array('Another action', '#'),
+                    array('Something else here', '#'),
+                    
+                    array(Navigation::DIVIDER),
+                    
+                    array(Navigation::HEADER, 'Nav header'),
+                    array('Separated link', '#'),
+                    array('One more separated link', '#'),
+                  )
+                )
+              );
+
+      $navlinkr = array(
+            array(Navigation::VERTICAL_DIVIDER),
+
+            array('About', $url.'/about'),
+            array('Login', $url.'/admin/login'),
+          );
+
+      echo Navbar::create()
+        ->with_brand( 'LimeBlack', $url.'/home' )
+        ->with_menus( Navigation::links( $navlink ) )
+        ->with_menus( Navigation::links( $navlinkr ),
+            array('class' => 'pull-right')
+        );
+    ?>
+@endsection
