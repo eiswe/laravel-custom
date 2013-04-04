4e5d4a91c4da2d76e65b7fdfcc8de3ead6b90a11
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-04 18:34:27 +0200

    added new menu to admin area!

--- bundles/admin/views/dashboard/emacs.blade.php
+++ bundles/admin/views/dashboard/emacs.blade.php
@@ -1,42 +1,4 @@
-@layout('layouts.default') 
-
-@section('topnav')
-    <?php
-        $url = URL::base(); // http://laravel.dev       //   return the Base URL for Developing from different Servers
-        
-        echo Navbar::create()
-            ->with_brand( 'LimeBlack', $url.'/home' )
-            ->with_menus( Navigation::links( 
-                array(
-                  array('Home', $url.'/home'),
-                  array('David', $url.'/david'),
-                  array('Paolo', $url.'/paolo'),
-                  array('Kazo', $url.'/kazo'),
-                  array( 'Dropdown', '#', false, false, array(
-                      array('Action', '#'),
-                      array('Another action', '#'),
-                      array('Something else here', '#'),
-                      
-                      array(Navigation::DIVIDER),
-                      
-                      array(Navigation::HEADER, 'Nav header'),
-                      array('Separated link', '#'),
-                      array('One more separated link', '#'),
-                    )
-                  )
-                ) 
-            ))
-          ->with_menus( Navigation::links( array(
-              array('Admin', $url.'/admin', true),
-              array(Navigation::VERTICAL_DIVIDER),
-
-              array('About', $url.'/about'),
-              array('Logout', $url.'/admin/logout'),
-            )),
-            array('class' => 'pull-right')
-          );
-    ?>
-@endsection
+@layout('layouts.navi')
 
 @section('subnav')
 	<?php 
