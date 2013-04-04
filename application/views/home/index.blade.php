ca4b3e2162fbf5044f98b13dda104a98b9dcd398
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-04 14:35:23 +0200

    must be consequent xD

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,46 +1,6 @@
 @layout('layouts.navi')
 
-@section('topnav')
 
-  <?php 
-      $url = URL::base(); // http://laravel.dev       //   return the Base URL for Developing from different Servers
-  /*
-      
-      $navlink = array(
-                array('Home', $url.'/home', true),
-                array('David', $url.'/david'),
-                array('Paolo', $url.'/paolo'),
-                array('Kazo', $url.'/kazo'),
-                array( 'Dropdown', '#', false, false, array(
-                    array('Action', '#'),
-                    array('Another action', '#'),
-                    array('Something else here', '#'),
-                    
-                    array(Navigation::DIVIDER),
-                    
-                    array(Navigation::HEADER, 'Nav header'),
-                    array('Separated link', '#'),
-                    array('One more separated link', '#'),
-                  )
-                )
-              );
-
-      $navlinkr = array(
-            array(Navigation::VERTICAL_DIVIDER),
-
-            array('About', $url.'/about'),
-            array('Login', $url.'/admin/login'),
-          );
-
-      echo Navbar::create()
-        ->with_brand( 'LimeBlack', $url.'/home' )
-        ->with_menus( Navigation::links( $navlink ) )
-        ->with_menus( Navigation::links( $navlinkr ),
-            array('class' => 'pull-right')
-        );
-        */
-    ?>
-@endsection
 
 @section('content')
 
@@ -55,6 +15,7 @@
         <p>Checkout Davids news!</p>
         <br />
             <?php 
+      $url = URL::base(); // http://laravel.dev       //   return the Base URL for Developing from different Servers
                 print '<p><a href="'.$url.'/david/news" class="btn btn-primary btn-large">To News!</a></p>';
             ?>   
 	    </div>
