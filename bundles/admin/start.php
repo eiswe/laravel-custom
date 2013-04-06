de87a1aac1c2125ef44c71f262eb3e631e38ea17
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-07 00:16:12 +0200

    pushed models to home bundle

--- bundles/admin/start.php
+++ bundles/admin/start.php
@@ -4,12 +4,8 @@
 	    'Admin_Base_Controller' => Bundle::path('admin').'controllers/base.php',
 	));
 
-	Autoloader::directories(array(
-		Bundle::path('admin').'models',
-	));
-
 	Autoloader::namespaces(array(
-	    //'Admin\Models' => Bundle::path('admin').'models',
+	    'Admin\Models' => Bundle::path('admin').'models',
 	    'Admin\Libraries' => Bundle::path('admin').'libraries',
 	));
 
