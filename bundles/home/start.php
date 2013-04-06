f98c02b17ae179dc658d490bb7dbb1309ab7335f
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-06 23:21:02 +0200

    added nessesary of models to start.php

--- bundles/home/start.php
+++ bundles/home/start.php
@@ -1,14 +1,14 @@
 <?php
 
-	Autoloader::map(array(
-	    'Home_Base_Controller' => Bundle::path('home').'controllers/base.php',
-	));
+	// Autoloader::map(array(
+	//     'Home_Base_Controller' => Bundle::path('home').'controllers/base.php',
+	// ));
 
-/*
 	Autoloader::namespaces(array(
 	    'Home\Models' => Bundle::path('home').'models',
-	    'Home\Libraries' => Bundle::path('home').'libraries',
 	));
+/*
+	    'Home\Libraries' => Bundle::path('home').'libraries',
 
 	Auth::extend('adminauth', function(){
 		return new Admin\Libraries\AdminAuth;
