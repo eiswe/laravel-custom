86fe2b5c0638af30c2549549b4e412668157f9fd
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-06 23:22:37 +0200

    moved page model to home bundle

---
+++ bundles/home/models/page.php
@@ -0,0 +1,15 @@
+<?php 
+class Page extends Eloquent {
+
+	public static $table = 'pages';
+	public static $timestamps = true;
+
+	public function admin() {
+		return $this->belongs_to('Admin');
+	}
+	
+	// public function userid() {
+	// 	return $this->has_one('Admin', 'userid');
+	// }
+	
+}
