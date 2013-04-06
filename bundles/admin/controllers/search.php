70e7e19ef2ad656efe48b9c80db2346dca6da3c0
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-06 14:37:22 +0200

    try to make search controller usable for tests xD

--- bundles/admin/controllers/search.php
+++ bundles/admin/controllers/search.php
@@ -4,17 +4,17 @@ class Admin_Search_Controller extends Admin_Base_Controller {
 
     public function get_index(){
 
-    	$Cards = card::all();   // lets load all cards exist in Database
+    	// $Cards = card::all();   // lets load all cards exist in Database
 
-	   	foreach ( $Cards as $key => $value ) {
-            $cardlist[] = array(                            // get all users to fetch username from id
-                $value->testdate,
-            );	   		
-        }
+	   	// foreach ( $Cards as $key => $value ) {
+     //        $cardlist[] = array(                            // get all users to fetch username from id
+     //            $value->testdate,
+     //        );	   		
+     //    }
 
         return View::make( 'admin::dashboard.search' )->with( 'title', 'Search for a Card' )
-        	->with( 'card', $Cards )
-        	->with( 'date', $cardlist )
+        	//->with( 'card', $Cards )
+        	//->with( 'date', $cardlist )
         ;
     }
 }
