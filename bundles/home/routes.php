422b36fd4d58d3595d740343d413c3d8a3394e6c
DeathPoison@Home <deathpoison.dc@gmail.com>
2013-04-07 01:04:40 +0200

    disable using eloquent in bundle routes...

--- bundles/home/routes.php
+++ bundles/home/routes.php
@@ -21,7 +21,7 @@ Route::get('/(:any)/news', function($any){ 						// if nothing match use any! - 
 			$uid = $key->admin_id;
 		}
         $news = Page::where('admin_id', '=', $uid )->get();
-		$news = Admin::find( $uid )->page()->get();    //all();  // cant use eloquent.. :'( - also cant autoload admin models...         ! cant reach admin class!
+		//$news = Admin::find( $uid )->page()->get();    //all();  // cant use eloquent.. :'( - also cant autoload admin models...         ! cant reach admin class!
 
 		return View::make('admin::show.news')
 			->with('title', 'LimeBlack - '.$any.' Index')
